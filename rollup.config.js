4731cdf1fd728c819dcb4b9bfe9865a1fcce6be1
Allohamora <herman2000@yandex.ru>
2021-12-10 15:11:55 +0200

    chore: add typescript, rollup and base scripts

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'rollup';
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import shebang from 'rollup-plugin-preserve-shebang';
+import pkg from './package.json';
+
+export default defineConfig({
+  input: pkg.main,
+  output: {
+    file: pkg.bin,
+    sourcemap: true,
+    format: 'cjs'
+  },
+  external: pkg.dependencies ? Object.keys(pkg.dependencies) : [],
+  plugins: [
+    typescript(),
+    commonjs(),
+    json(),
+    shebang()
+  ]
+});
