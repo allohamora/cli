1fd2cd926ed406faf084ee60c268306a06e7697c
Allohamora <herman2000@yandex.ru>
2022-01-23 11:58:53 +0200

    fix: add format to release-worflow content

--- src/libs/string.ts
+++ src/libs/string.ts
@@ -1,3 +1,5 @@
+import { compose } from './fp';
+
 export const kebablize = (camel: string) => {
   return camel
     .split('')
@@ -17,3 +19,24 @@ export const camelize = (kebab: string) => {
 
   return `${first}${capilazedRest}`;
 };
+
+export const buildTemplate = (strings: TemplateStringsArray, ...values: unknown[]) => {
+  const result = strings.reduce((state, string, index) => {
+    const value = values[index] ?? '';
+
+    return `${state}${string}${value}`;
+  });
+
+  return result;
+};
+
+export const templateWithFormat = (...funcs: Array<(value: string) => string>) => {
+  return (...params: Parameters<typeof buildTemplate>) => {
+    const builded = buildTemplate(...params);
+
+    return compose(...funcs)(builded);
+  };
+};
+
+export const removeTabOnEachLine = (string: string) => string.replace(/\n(  |\t)/g, '\n');
+export const trim = (string: string) => string.trim();
