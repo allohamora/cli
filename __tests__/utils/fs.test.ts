f72fafa8bf63d1bb218fd3bd11c02735a37c15a6
Allohamora <herman2000@yandex.ru>
2022-01-26 10:19:15 +0200

    test: add should words to fs test

--- __tests__/utils/fs.test.ts
+++ __tests__/utils/fs.test.ts
@@ -25,14 +25,14 @@ beforeEach(() => {
 });
 
 describe('existsInRoot', () => {
-  test('returns true if file exists at root', async () => {
+  test('should return true if file exists at root', async () => {
     const actual = await existsInRoot(rootFile);
     const expected = true;
 
     expect(actual).toBe(expected);
   });
 
-  test("returns false if file doesn't exist at root", async () => {
+  test("should return false if file doesn't exist at root", async () => {
     const actual = await existsInRoot('__test__.json');
     const expected = false;
 
@@ -41,7 +41,7 @@ describe('existsInRoot', () => {
 });
 
 describe('addDirToRootIfNotExists', () => {
-  test("creates dir at root if it doesn't exist", async () => {
+  test("should create dir at root if it doesn't exist", async () => {
     const dir = '__test__';
     const expected = rootPath(dir);
 
@@ -50,7 +50,7 @@ describe('addDirToRootIfNotExists', () => {
     expect(fspMocked.mkdir).toBeCalledWith(expected);
   });
 
-  test("doesn't create dir at root if it exists", async () => {
+  test('should not create dir at root if it exists', async () => {
     const expected = rootPath(rootDir);
 
     await addDirToRootIfNotExists(rootDir);
@@ -61,7 +61,7 @@ describe('addDirToRootIfNotExists', () => {
 });
 
 describe('addFileToRoot', () => {
-  test('adds file to root', async () => {
+  test('should add file to root', async () => {
     const fileName = '__test__.json';
     const content = '__test__';
 
@@ -86,7 +86,7 @@ describe('addJsonFileToRoot', () => {
     global.JSON = originalJSON;
   });
 
-  test('adds json file to root', async () => {
+  test('should add json file to root', async () => {
     const fileName = '__test__.json';
     const content = {};
 
