55d3b8261fa0ce05d3c23f16c3d258076200f091
Allohamora <herman2000@yandex.ru>
2022-01-26 15:11:46 +0200

    test: add createConfigState tests

--- __tests__/utils/state.test.ts
+++ __tests__/utils/state.test.ts
@@ -1,11 +1,13 @@
-import { createTypeState } from 'src/utils/state';
+import { createConfigState, createTypeState } from 'src/utils/state';
 
 describe('createTypeState', () => {
   const types = ['1', '2'];
-  let [getType, setType] = createTypeState(types);
+  const createNewTypeState = () => createTypeState(types);
+
+  let [getType, setType] = createNewTypeState();
 
   beforeEach(() => {
-    [getType, setType] = createTypeState(types);
+    [getType, setType] = createNewTypeState();
   });
 
   test('should return type', () => {
@@ -22,5 +24,40 @@ describe('createTypeState', () => {
     setType(newType);
 
     expect(getType()).toBe(newType);
+  });
+});
+
+describe('createConfigState', () => {
+  const types = ['1', '2', '3'];
+  const createNewTypeState = () => createTypeState(types);
+  let state = createNewTypeState();
+  let [getType, setType] = state;
+
+  const configValues = { default: 'default', '1': '1' };
+  const createNewConfigState = () => createConfigState(state, configValues);
+  let [getConfig] = createNewConfigState();
+
+  afterEach(() => {
+    state = createNewTypeState();
+    [getType, setType] = state;
+    [getConfig] = createNewConfigState();
+  });
+
+  test('should return selected config value', () => {
+    setType(types[0]);
+
+    const actual = getConfig();
+    const expected = configValues['1'];
+
+    expect(actual).toBe(expected);
+  });
+
+  test('should return default config if selected not found', () => {
+    setType(types[1]);
+
+    const actual = getConfig();
+    const expected = configValues.default;
+
+    expect(actual).toBe(expected);
   });
 });
