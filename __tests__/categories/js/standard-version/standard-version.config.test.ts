4134cfae73c0ef630916304900cc74385ad4970f
Allohamora <herman2000@yandex.ru>
2022-01-29 16:58:09 +0200

    feat: add tests for standard-version

---
+++ __tests__/categories/js/standard-version/standard-version.config.test.ts
@@ -0,0 +1,4 @@
+import { getConfig } from 'src/categories/js/standard-version/standard-version.config';
+import { expectJsConfig } from '__tests__/test-utils/js-config';
+
+expectJsConfig(getConfig);
