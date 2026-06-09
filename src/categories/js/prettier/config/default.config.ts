b21209d821a15c5006a60df68e89fdc91f0c9560
allohamora <herman30698@gmail.com>
2026-06-09 20:49:40 +0300

    refactor: fix node: in names

--- src/categories/js/prettier/config/default.config.ts
+++ src/categories/js/prettier/config/default.config.ts
@@ -1,22 +1,29 @@
 export const defaultConfig = {
   config: {
     semi: true,
-    trailingComma: 'all',
+    trailingComma: "all",
     singleQuote: true,
     printWidth: 120,
     tabWidth: 2,
     overrides: [
       {
-        files: '*.yml',
+        files: "*.yml",
         options: {
           singleQuote: false,
         },
       },
     ],
   },
-  ignore: ['dist', 'node_modules', 'public', '.husky', 'package-lock.json', 'coverage'],
+  ignore: [
+    "dist",
+    "node_modules",
+    "public",
+    ".husky",
+    "package-lock.json",
+    "coverage",
+  ],
   scripts: [
-    { name: 'format', script: 'prettier . --check' },
-    { name: 'format:fix', script: 'prettier --write .' },
+    { name: "format", script: "prettier . --check" },
+    { name: "format:fix", script: "prettier --write ." },
   ],
 };
--- src/categories/js/stylelint/stylelint.config.ts
+++ src/categories/js/stylelint/stylelint.config.ts
@@ -1,6 +1,6 @@
 import { jsCategoryState } from '#src/states/categories.ts';
 import { defaultConfig } from '#src/categories/js/stylelint/config/default.config.ts';
-import { reactTsConfig } from '#src/categories/js/stylelint/config/react:ts.config.ts';
+import { reactTsConfig } from '#src/categories/js/stylelint/config/react-ts.config.ts';
 
 export const [getConfig] = jsCategoryState.useConfigState({
   default: defaultConfig,
