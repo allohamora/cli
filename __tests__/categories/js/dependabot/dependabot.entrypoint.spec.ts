26b2dfde76328f621571f3c4f42d00c50ee0ae15
allohamora <herman30698@gmail.com>
2026-06-10 17:02:24 +0300

    test: fix entypoint typo

--- __tests__/categories/js/dependabot/dependabot.entrypoint.spec.ts
+++ __tests__/categories/js/dependabot/dependabot.entrypoint.spec.ts
@@ -2,7 +2,7 @@ import { fileSystem } from '#__tests__/setup-test-context.ts';
 import { defaultConfig } from '#src/categories/js/dependabot/config/default.config.ts';
 import { dependabot } from '#src/categories/js/dependabot/dependabot.entrypoint.ts';
 
-describe('dependabot.entypoint', () => {
+describe('dependabot.entrypoint', () => {
   describe('dependabot', () => {
     it('writes the default dependabot config under .github', async () => {
       await dependabot();
