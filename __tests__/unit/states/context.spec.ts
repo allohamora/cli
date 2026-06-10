cd8aca29273fe79856f1ee01b7e7000a9a0239fb
allohamora <herman30698@gmail.com>
2026-06-10 18:29:20 +0300

    test: remove test duplication

--- __tests__/unit/states/context.spec.ts
+++ __tests__/unit/states/context.spec.ts
@@ -1,23 +1,8 @@
 import { setInstalling, getInstalling } from '#src/states/context.ts';
 
 describe('context', () => {
-  const expectSetAndGet = () => {
-    const installing = ['__test__'];
-
-    setInstalling(installing);
-
-    const actual = getInstalling();
-    const expected = installing;
-
-    expect(actual).toBe(expected);
-  };
-
   beforeEach(() => {
     setInstalling([]);
-  });
-
-  describe('setInstalling', () => {
-    it('sets installing', expectSetAndGet);
   });
 
   describe('getInstalling', () => {
@@ -27,7 +12,18 @@ describe('context', () => {
 
       expect(actual).toEqual(expected);
     });
+  });
 
-    it('returns installing', expectSetAndGet);
+  describe('setInstalling and getInstalling', () => {
+    it('sets and gets installing', () => {
+      const installing = ['__test__'];
+
+      setInstalling(installing);
+
+      const actual = getInstalling();
+      const expected = installing;
+
+      expect(actual).toBe(expected);
+    });
   });
 });
